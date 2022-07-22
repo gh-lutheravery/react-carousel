f8fc58cb5ca44cb44274ce5fbd1dd0f9d3c3e8a7
gh-charles <blakos244@gmail.com>
2022-07-21 19:34:09 -0500

    implemented carousel and constructor

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import './index.css';
 
 var LISTCOUNT = 30;
 var ITEMSPERPAGECOUNT = 30;
+
+class Carousel extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemList: [{
+        items: initPaginatedList()
+      }],
+      pageNum: 0
+    };
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
